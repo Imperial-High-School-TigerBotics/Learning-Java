b0a044e4b332f36553e42fe3e3119dae2a6b8d18
Awesome Sauce <78565561+Awesome-Sauces@users.noreply.github.com>
2022-12-01 21:45:02 -0800

    Add files via upload

---
+++ Lesson1/HelloFunctions.java
@@ -0,0 +1,12 @@
+package Lesson1;
+
+public class HelloFunctions {
+
+    public HelloFunctions(String string){
+        Say(string);
+    }
+
+    public void Say(String Sentence) {
+        System.out.println("Lesson #1 -> " + Sentence);
+    }
+}
