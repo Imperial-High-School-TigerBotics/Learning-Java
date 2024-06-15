3b426d3c8c43fc60d78d2e204eab705c90290c5b
mando8119 <armando.g.barron1@gmail.com>
2024-06-15 09:55:48 -0700

    Created Executable Java Program for Lesson on Monday

---
+++ Lesson1/Lesson_1.java
@@ -0,0 +1,11 @@
+package Lesson1;
+
+public class Lesson_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // new HelloWorld();
+        // new HelloFunctions("Hello Functions!");
+        // new HelloLoops();
+    }
+}
